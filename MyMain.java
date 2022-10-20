514e87ae83e6c2b5ecf17f7379d45357df41d0a9
munnasharma2001 <116312104+munnasharma2001@users.noreply.github.com>
2022-10-21 02:01:04 +0530

    MyMain

--- MyMain.java
+++ MyMain.java
@@ -1,12 +1,8 @@
 package main;
 
-import infix.postfix.InfixToPostfix;
-
 public class MyMain {
     public static void main(String[] args) {
-        InfixToPostfix converter = new InfixToPostfix();
-        String infix = "2^2^3";
-        String postfix = converter.infixPostfix(infix);
-        System.out.println("postfix => " + postfix);
+        //create binary search tree
+        // and perform insert, search, traversal, delete
     }
 }
